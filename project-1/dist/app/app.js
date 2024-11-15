5b9dd59d1b730d32014e83e3b85143bc670e6cc3
MacBook Pro <naym100m@gmail.com>
2024-11-15 18:29:15 +0600

    [compleate] module in express.js

--- project-1/dist/app/app.js
+++ project-1/dist/app/app.js
@@ -10,20 +10,36 @@ app.use(express_1.default.json());
 app.use(express_1.default.text());
 const userRouter = express_1.default.Router();
 app.use("/api/v1/users", userRouter);
-userRouter.get("/create-users", (req, res) => {
-    const user = req.body;
-    console.log(user);
-    res.json({
-        success: true,
-        message: "User creating successfully done!",
-        name: "naYm",
-    });
+userRouter.get("/create-users", (req, res, next) => {
+    try {
+        const user = req.body;
+        console.log(user);
+        res.json({
+            success: true,
+            message: "User creating successfully done!",
+            name: "naYm",
+        });
+    }
+    catch (error) {
+        console.log(error);
+        next(error);
+    }
 });
 // Middleware
 const logger = (req, res, next) => {
     console.log(req.url, req.method, req.hostname);
     next();
 };
+//  Global error handler middleware
+app.use((error, req, res, next) => {
+    console.log(error);
+    if (error) {
+        res.status(400).json({
+            success: false,
+            message: "Something went wrong!",
+        });
+    }
+});
 app.get("/", logger, (req, res) => {
     res.send("Hello Dev World! Whats going on?");
 });
--- project-1/src/app/app.ts
+++ project-1/src/app/app.ts
@@ -1,4 +1,5 @@
-import express, { NextFunction, Request, Response } from "express";
+import { error } from "console";
+import express, { NextFunction, request, Request, Response } from "express";
 const app = express();
 
 // Parser
@@ -8,21 +9,41 @@ app.use(express.text());
 const userRouter = express.Router();
 app.use("/api/v1/users", userRouter);
 
-userRouter.get("/create-users", (req: Request, res: Response) => {
-  const user = req.body;
-  console.log(user);
-  res.json({
-    success: true,
-    message: "User creating successfully done!",
-    name: "naYm",
-  });
-});
+userRouter.get(
+  "/create-users",
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const user = req.body;
+      console.log(user);
+      res.json({
+        success: true,
+        message: "User creating successfully done!",
+        name: "naYm",
+      });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+);
 
 // Middleware
 const logger = (req: Request, res: Response, next: NextFunction) => {
   console.log(req.url, req.method, req.hostname);
   next();
 };
+
+//  Global error handler middleware
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  console.log(error);
+
+  if (error) {
+    res.status(400).json({
+      success: false,
+      message: "Something went wrong!",
+    });
+  }
+});
 
 app.get("/", logger, (req: Request, res: Response) => {
   res.send("Hello Dev World! Whats going on?");
