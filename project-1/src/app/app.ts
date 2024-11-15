8a58e7d62ad9950b25a66754b8236b412ba339e2
MacBook Pro <naym100m@gmail.com>
2024-11-15 17:12:23 +0600

    [learn] Middleware

--- project-1/src/app/app.ts
+++ project-1/src/app/app.ts
@@ -1,9 +1,23 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// Parser
+app.use(express.json());
+app.use(express.text());
+
+// Middleware
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url, req.method, req.hostname);
+  next();
+};
+
+app.get("/", logger, (req: Request, res: Response) => {
+  res.send("Hello Dev World! Whats going on?");
+});
+
+app.post("/", (res: Response, req: Request) => {
+  console.log(req.body);
+  res.send();
 });
 
 export default app;
