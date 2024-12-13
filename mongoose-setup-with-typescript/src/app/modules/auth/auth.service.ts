e2b9a2d1ea369cd779f79f13a155365fed999fb8
MacBook Pro <naym100m@gmail.com>
2024-12-13 21:35:40 +0600

    [learning a] 17-2 Implement business logic validation in auth service

--- mongoose-setup-with-typescript/src/app/modules/auth/auth.service.ts
+++ mongoose-setup-with-typescript/src/app/modules/auth/auth.service.ts
@@ -1,14 +1,36 @@
-import { User } from "../users/user.model"
-import { TLoginUser } from "./auth.interface"
-
+import { StatusCodes } from 'http-status-codes';
+import AppError from '../../errors/appError';
+import { User } from '../users/user.model';
+import { TLoginUser } from './auth.interface';
+import bcrypt from 'bcrypt';
 
 const loginUser = async (payload: TLoginUser) => {
+  // Check user exist or no!
+  const isUserExists = await User.findOne({ id: payload?.id });
 
-    // const user = await User
+  if (!isUserExists) {
+    throw new AppError(StatusCodes.NOT_FOUND, 'This user is not found');
+  }
 
-}
+  // check user is deleted or not!
+  if (isUserExists?.isDeleted) {
+    throw new AppError(StatusCodes.FORBIDDEN, 'This user is already deleted');
+  }
+  // check user is blocked or not!
+  if (isUserExists?.status === 'blocked') {
+    throw new AppError(StatusCodes.FORBIDDEN, 'This user is blocked!');
+  }
 
+  // Checking if password is matching?
+
+  const isPasswordMatch = await bcrypt.compare(
+    payload?.password,
+    isUserExists?.password,
+  );
+
+  
+};
 
 export const AuthServices = {
-    loginUser
-}
+  loginUser,
+};
