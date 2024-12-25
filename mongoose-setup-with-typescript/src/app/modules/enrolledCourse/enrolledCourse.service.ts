58d831f0e86a8aa6ebfe4c98df857a6b095b0b8d
MacBook Pro <naym100m@gmail.com>
2024-12-25 12:00:38 +0600

    20-3 Add business logic validation into enrolled course service

--- mongoose-setup-with-typescript/src/app/modules/enrolledCourse/enrolledCourse.service.ts
+++ mongoose-setup-with-typescript/src/app/modules/enrolledCourse/enrolledCourse.service.ts
@@ -1,22 +1,46 @@
+import { StatusCodes } from 'http-status-codes';
+import AppError from '../../errors/AppError';
+import { OfferedCourse } from '../offeredCourse/offeredCourse.model';
 import { TEnrolledCourse } from './enrolledCourse.interface';
 import EnrolledCourse from './enrolledCourse.model';
+import { StudentModel } from '../students/studentSchema';
 
 const createEnrollCourseFromDB = async (
   userId: string,
   payload: TEnrolledCourse,
 ) => {
   /**
-   * Step 1: check if the semester registration id is exists!
-   * Step 2: check if the academic faculty id is exists!
-   * Step 3: check if the academic department id is exists!
-   * Step 4: check if the course id is exists!
-   * Step 5: check if the faculty id is exists!
-   * Step 6: check if the department is belong to the  faculty
-   * Step 7: check if the same offered course same section in same registered semester exists
-   * Step 8: get the schedules of the faculties
-   * Step 9: check if the faculty is available at that time. If not then throw error
-   * Step 10: create the offered course
+   * Step1: Check if the offered cousres is exists
+   * Step2: Check if the student is already enrolled
+   * Step3: Check if the max credits exceed
+   * Step4: Create an enrolled course
    */
+
+  const { offeredCourse } = payload;
+
+  const isOfferedCourseExists = await OfferedCourse.findById(offeredCourse);
+  if (!isOfferedCourseExists) {
+    throw new AppError(StatusCodes.NOT_FOUND, 'Offered course not found');
+  }
+
+  if (isOfferedCourseExists?.maxCapacity <= 0) {
+    throw new AppError(StatusCodes.BAD_REQUEST, 'Course is full');
+  }
+
+  const student = await StudentModel.findOne({ id: userId }).select('id');
+  if (!student) {
+    throw new AppError(StatusCodes.NOT_FOUND, 'Student not found');
+  }
+
+  const isStudentEnrolled = await EnrolledCourse.findOne({
+    semesterRegistration: isOfferedCourseExists?.semesterRegistration,
+    offeredCourse,
+    student: student._id,
+  });
+
+  if (isStudentEnrolled) {
+    throw new AppError(StatusCodes.BAD_REQUEST, 'Student already enrolled');
+  }
 
   const result = await EnrolledCourse.create(userId);
 
