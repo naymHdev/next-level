3b1c1cbfb7f480620a294d8cbb84892e36f9e284
MacBook Pro <naym100m@gmail.com>
2024-12-27 18:39:42 +0600

    update and refoctoring codes

--- mongoose-setup-with-typescript/src/app/modules/academicSemester/academicSemester.route.ts
+++ mongoose-setup-with-typescript/src/app/modules/academicSemester/academicSemester.route.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import { AcademicSemesterController } from './acedemicSemester.controller';
 import { validateRequest } from '../../middlewares/validateRequest';
 import { AcademicSemesterValidations } from './academicSemester.validation';
+import { auth } from '../../middlewares/auth';
 
 const router = express.Router();
 
 router.post(
   '/create-academic-semester',
+  auth('admin', 'superAdmin', 'faculty'),
   validateRequest(
     AcademicSemesterValidations.createAcademicSemesterValidationSchema,
   ),
@@ -15,16 +17,22 @@ router.post(
 
 router.get(
   '/:semesterId',
+  auth('admin', 'superAdmin', 'faculty', 'student'),
   AcademicSemesterController.getSingleAcademicSemester,
 );
 
 router.patch(
   '/:semesterId',
+  auth('admin', 'superAdmin', 'faculty', 'student'),
   validateRequest(
     AcademicSemesterValidations.updateAcademicSemesterValidationSchema,
   ),
   AcademicSemesterController.updateAcademicSemester,
 );
 
-router.get('/', AcademicSemesterController.getAllAcademicSemesters);
+router.get(
+  '/',
+  auth('admin', 'superAdmin', 'faculty'),
+  AcademicSemesterController.getAllAcademicSemesters,
+);
 export const AcademicSemesterRoutes = router;
--- mongoose-setup-with-typescript/src/app/modules/semesterRegistration/semesterRegistration.route.ts
+++ mongoose-setup-with-typescript/src/app/modules/semesterRegistration/semesterRegistration.route.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import { validateRequest } from '../../middlewares/validateRequest';
 import { SemesterRegistrationValidations } from './semesterRegistration.validation';
 import { SemesterRegistrationController } from './semesterRegistration.controller';
+import { auth } from '../../middlewares/auth';
 
 const router = express.Router();
 
 router.post(
   '/create-semester-registration',
+  auth('admin', 'superAdmin', 'faculty'),
   validateRequest(
     SemesterRegistrationValidations.createSemesterRegistrationValidationSchema,
   ),
@@ -15,11 +17,13 @@ router.post(
 
 router.get(
   '/:id',
+  auth('admin', 'superAdmin', 'faculty'),
   SemesterRegistrationController.getSingleSemesterRegistration,
 );
 
 router.patch(
   '/:id',
+  auth('admin', 'superAdmin', 'faculty'),
   validateRequest(
     SemesterRegistrationValidations.updateSemesterRegistrationValidationSchema,
   ),
@@ -28,14 +32,20 @@ router.patch(
 
 router.get(
   '/:id',
+  auth('admin', 'superAdmin', 'faculty'),
   SemesterRegistrationController.getSingleSemesterRegistration,
 );
 
 router.delete(
   '/:id',
+  auth('admin', 'superAdmin', 'faculty'),
   SemesterRegistrationController.deleteSemesterRegistration,
 );
 
-router.get('/', SemesterRegistrationController.getAllSemesterRegistrations);
+router.get(
+  '/',
+  auth('admin', 'superAdmin', 'faculty'),
+  SemesterRegistrationController.getAllSemesterRegistrations,
+);
 
 export const semesterRegistrationRoutes = router;
