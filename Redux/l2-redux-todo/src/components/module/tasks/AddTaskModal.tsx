3d8a9f50ea792d30ae4e9644c471ab7d70a7707d
MacBook Pro <naym100m@gmail.com>
2025-01-02 21:39:35 +0600

    [refactor] refactoring modal

---
+++ Redux/l2-redux-todo/src/components/module/tasks/AddTaskModal.tsx
@@ -0,0 +1,59 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+
+export function AddTaskModal() {
+  const form = useForm();
+
+  const onsubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Add Task</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onsubmit)}>
+            <FormField
+              control={form.control}
+              name="Title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel />
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- Redux/l2-redux-todo/src/pages/tasks/Task.tsx
+++ Redux/l2-redux-todo/src/pages/tasks/Task.tsx
@@ -1,3 +1,4 @@
+import { AddTaskModal } from "@/components/module/tasks/AddTaskModal";
 import TaskCard from "@/components/module/tasks/TaskCard";
 import { selectTasks } from "@/redux/features/task/taskSlice";
 import { useAppSelector } from "@/redux/hook";
@@ -8,7 +9,10 @@ const Task = () => {
   return (
     <>
       <div className=" mx-auto max-w-7xl px-5 mt-20">
-        <h3 className=" font-bold text-lg">My Task:</h3>
+        <div className=" flex items-center justify-between">
+          <h3 className=" font-bold text-lg">My Task:</h3>
+          <AddTaskModal />
+        </div>
 
         <div className=" space-y-5 mt-5">
           {tasks?.map((task) => (
