992dc48addced6a160c4b86b64e7980586eaa534
MacBook Pro <naym100m@gmail.com>
2025-01-03 00:06:19 +0600

    [refactor] add task modal form

--- Redux/l2-redux-todo/src/components/module/tasks/AddTaskModal.tsx
+++ Redux/l2-redux-todo/src/components/module/tasks/AddTaskModal.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
@@ -15,14 +16,26 @@ import {
   FormLabel,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Calendar } from "@/components/ui/calendar";
+import { Textarea } from "@/components/ui/textarea";
 import { useForm } from "react-hook-form";
+import React from "react";
 
 export function AddTaskModal() {
   const form = useForm();
 
-  const onsubmit = (data) => {
+  const onsubmit = (data: Record<string, unknown>) => {
     console.log(data);
   };
+
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
 
   return (
     <Dialog>
@@ -30,25 +43,82 @@ export function AddTaskModal() {
         <Button variant="outline">Add Task</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
+        <DialogDescription className=" sr-only">
+          Fill up this form to add the task
+        </DialogDescription>
         <DialogHeader>
           <DialogTitle>Edit profile</DialogTitle>
         </DialogHeader>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onsubmit)}>
+          <form className=" space-y-3" onSubmit={form.handleSubmit(onsubmit)}>
             <FormField
               control={form.control}
-              name="Title"
+              name="title"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel />
+                  <FormLabel>Title</FormLabel>
                   <FormControl>
-                    <Input {...field} />
+                    <Input {...field} value={field.value || ""} />
                   </FormControl>
                 </FormItem>
               )}
             />
 
-            <DialogFooter>
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea {...field} value={field.value || ""} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="dueDate"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Due Date</FormLabel>
+                  <FormControl>
+                    <Calendar
+                      {...field}
+                      mode="single"
+                      selected={date}
+                      onSelect={setDate}
+                      className="rounded-md border shadow"
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="priority"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Priority</FormLabel>
+                  <FormControl>
+                    <Select {...field} value={field.value || ""}>
+                      <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Theme" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="light">Light</SelectItem>
+                        <SelectItem value="dark">Dark</SelectItem>
+                        <SelectItem value="system">System</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <DialogFooter className=" mt-5">
               <Button type="submit">Save changes</Button>
             </DialogFooter>
           </form>
