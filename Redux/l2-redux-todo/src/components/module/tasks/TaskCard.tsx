59fc91ca0e244000e29f272538b7da58c897933e
MacBook Pro <naym100m@gmail.com>
2025-01-03 22:40:23 +0600

    [create] a task deleted and check tagoole function

--- Redux/l2-redux-todo/src/components/module/tasks/TaskCard.tsx
+++ Redux/l2-redux-todo/src/components/module/tasks/TaskCard.tsx
@@ -3,12 +3,19 @@ import { cn } from "@/lib/utils";
 import { ITask } from "@/types/types";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Trash2 } from "lucide-react";
+import { useAppDispatch } from "@/redux/hook";
+import {
+  deleteTask,
+  toggleCompleteState,
+} from "@/redux/features/task/taskSlice";
 
 interface IProps {
   task: ITask;
 }
 
 const TaskCard = ({ task }: IProps) => {
+  const dispatch = useAppDispatch();
+
   return (
     <>
       <div className=" border px-5 py-3 rounded-md">
@@ -21,13 +28,26 @@ const TaskCard = ({ task }: IProps) => {
                 "bg-red-500": task.priority === "High",
               })}
             ></div>
-            <h1 className=" font-bold text-lg">{task.title}</h1>
+            <h1
+              className={cn("font-bold text-lg", {
+                "line-through": task.isCompleted,
+              })}
+            >
+              {task.title}
+            </h1>
           </div>
           <div className=" flex gap-3 items-center">
-            <Button variant="link" className=" p-0 text-red-500">
+            <Button
+              onClick={() => dispatch(deleteTask(task?.id))}
+              variant="link"
+              className=" p-0 text-red-500"
+            >
               <Trash2 />
             </Button>
-            <Checkbox />
+            <Checkbox
+              checked={task.isCompleted}
+              onClick={() => dispatch(toggleCompleteState(task?.id))}
+            />
           </div>
         </div>
         <p>{task.description}</p>
--- Redux/l2-redux-todo/src/redux/features/task/taskSlice.ts
+++ Redux/l2-redux-todo/src/redux/features/task/taskSlice.ts
@@ -8,30 +8,30 @@ interface IInitialState {
 
 const initialState: IInitialState = {
   tasks: [
-    // {
-    //   id: "109272918",
-    //   title: "initialiZe front-end",
-    //   description: "Create home page, and routing",
-    //   dueDate: "2025-11",
-    //   isCompleted: false,
-    //   priority: "High",
-    // },
-    // {
-    //   id: "0921373273",
-    //   title: "Create github repo",
-    //   description: "Create home page, and routing",
-    //   dueDate: "2025-13",
-    //   isCompleted: false,
-    //   priority: "Medium",
-    // },
-    // {
-    //   id: "982368327",
-    //   title: "Create Medium Blogs",
-    //   description: "Create home page, and routing",
-    //   dueDate: "2025-13",
-    //   isCompleted: false,
-    //   priority: "Low",
-    // },
+    {
+      id: "109272918",
+      title: "initialiZe front-end",
+      description: "Create home page, and routing",
+      dueDate: "2025-11",
+      isCompleted: false,
+      priority: "High",
+    },
+    {
+      id: "0921373273",
+      title: "Create github repo",
+      description: "Create home page, and routing",
+      dueDate: "2025-13",
+      isCompleted: false,
+      priority: "Medium",
+    },
+    {
+      id: "982368327",
+      title: "Create Medium Blogs",
+      description: "Create home page, and routing",
+      dueDate: "2025-13",
+      isCompleted: false,
+      priority: "Low",
+    },
   ],
 };
 
@@ -53,6 +53,20 @@ const taskSlice = createSlice({
       const taskData = createTask(action.payload);
       state.tasks.push(taskData);
     },
+
+    toggleCompleteState: (state, action: PayloadAction<string>) => {
+      console.log("action", action);
+
+      state.tasks.forEach((task) =>
+        task.id === action.payload
+          ? (task.isCompleted = !task.isCompleted)
+          : task
+      );
+    },
+
+    deleteTask: (state, action: PayloadAction<string>) => {
+      state.tasks = state.tasks.filter((task) => task.id !== action.payload);
+    },
   },
 });
 
@@ -60,6 +74,6 @@ export const selectTasks = (state: RootState) => {
   return state?.todo.tasks;
 };
 
-export const { addTask } = taskSlice.actions;
+export const { addTask, toggleCompleteState, deleteTask } = taskSlice.actions;
 
 export default taskSlice.reducer;
