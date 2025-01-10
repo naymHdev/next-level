ff626abdecb80106b71751a69984519d2ff7137b
MacBook Pro <naym100m@gmail.com>
2025-01-10 10:02:49 +0600

    created || role base dynamic sidebar

---
+++ phum-client/src/components/layout/Sidebar.tsx
@@ -0,0 +1,68 @@
+import Sider from "antd/es/layout/Sider";
+import { adminPaths } from "../../routes/admin.routes";
+import { siteBarItemsGenerator } from "../../utils/sitebarItemsGenerator";
+import { Menu } from "antd";
+import { facultyPaths } from "../../routes/faculty.routes";
+import { studentPaths } from "../../routes/student.routes";
+
+const userRole = {
+  ADMIN: "admin",
+  FACULTY: "faculty",
+  STUDENT: "student",
+};
+
+const Sidebar = () => {
+  const role = "faculty";
+  let sidebarItems;
+
+  switch (role) {
+    case userRole.ADMIN:
+      sidebarItems = siteBarItemsGenerator(adminPaths, userRole.ADMIN);
+      break;
+    case userRole.FACULTY:
+      sidebarItems = siteBarItemsGenerator(facultyPaths, userRole.FACULTY);
+      break;
+    case userRole.STUDENT:
+      sidebarItems = siteBarItemsGenerator(studentPaths, userRole.STUDENT);
+      break;
+
+    default:
+      break;
+  }
+
+  return (
+    <>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            textAlign: "center",
+            height: "4rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h1>PH Uni</h1>
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["4"]}
+          items={sidebarItems}
+        />
+      </Sider>
+    </>
+  );
+};
+
+export default Sidebar;
