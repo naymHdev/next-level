49bafc8570aa25f55f3c276b6a8966fd76d78664
MacBook Pro <naym100m@gmail.com>
2025-01-13 18:02:51 +0600

    [learn] how to set access token in baseapi and fatch data use redux toolkit

--- phum-client/src/pages/admin/academicManagement/AcademicSemester.tsx
+++ phum-client/src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,5 +1,11 @@
+import { useGetAllSemestersQuery } from "../../../redux/features/academicSemester/academicSemesterApi";
+
 const AcademicSemester = () => {
-  return <div>AcademicSemester</div>;
+  const { data } = useGetAllSemestersQuery(undefined);
+
+  console.log("data", data);
+
+  return <div>AcademicSemester </div>;
 };
 
 export default AcademicSemester;
