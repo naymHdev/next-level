66e301dd99eb2d36b0b46641bce7a6cb4037466a
MacBook Pro <naym100m@gmail.com>
2025-01-16 11:27:05 +0600

    [post data] functionality compleated in academic faculty

--- phum-client/src/pages/admin/academicManagement/AcademicFaculty.tsx
+++ phum-client/src/pages/admin/academicManagement/AcademicFaculty.tsx
@@ -1,11 +1,64 @@
+import { Table, TableColumnsType, TableProps } from "antd";
+import { useGetAllAcademicFacultyQuery } from "../../../redux/features/admin/academicFaculty.api";
+import { TQueryParam } from "../../../types";
+import { useState } from "react";
+import { TAcademicFaculty } from "../../../types/academicFaculty.type";
 
+export type TTableData = Pick<TAcademicFaculty, "name">;
 
 const AcademicFaculty = () => {
-    return (
-        <div>
-            AcademicFaculty
-        </div>
-    );
+  const [params, setParams] = useState<TQueryParam[] | undefined>(undefined);
+
+  const { data: academicFacultyData, isFetching } =
+    useGetAllAcademicFacultyQuery(params);
+
+  const tableData = academicFacultyData?.data?.map(({ _id, name }) => ({
+    key: _id,
+    name,
+  }));
+
+  console.log("tableData", tableData);
+
+  const columns: TableColumnsType<TTableData> = [
+    {
+      title: "Academic Faculty Name",
+      dataIndex: "name",
+      showSorterTooltip: { target: "full-header" },
+
+      // specify the condition of filtering result
+      // here is that finding the name started with `value`
+      onFilter: (value, record) => record.name.indexOf(value as string) === 0,
+      sorter: (a, b) => a.name.length - b.name.length,
+      sortDirections: ["descend"],
+    },
+  ];
+
+  const onChange: TableProps<TTableData>["onChange"] = (
+    _pagination,
+    filters,
+    _sorter,
+    extra
+  ) => {
+    if (extra.action === "filter") {
+      const queryParams: TQueryParam[] = [];
+      filters.name?.forEach((item) =>
+        queryParams.push({ name: "name", value: item })
+      );
+      setParams(queryParams);
+    }
+  };
+
+  return (
+    <>
+      <Table<TTableData>
+        loading={isFetching}
+        columns={columns}
+        dataSource={tableData}
+        onChange={onChange}
+        showSorterTooltip={{ target: "sorter-icon" }}
+      />
+    </>
+  );
 };
 
 export default AcademicFaculty;
--- phum-client/src/pages/admin/academicManagement/CreateAcademicFaculty.tsx
+++ phum-client/src/pages/admin/academicManagement/CreateAcademicFaculty.tsx
@@ -1,5 +1,56 @@
+import { Button, Col, Flex } from "antd";
+import PHForm from "../../../components/form/PHForm";
+import PHInput from "../../../components/form/PHInput";
+import { zodResolver } from "@hookform/resolvers/zod";
+import z from "zod";
+import { FieldValues, SubmitErrorHandler } from "react-hook-form";
+import { useAddAcademicFacultyMutation } from "../../../redux/features/admin/academicFaculty.api";
+import { toast } from "sonner";
+import { TResponse } from "../../../types";
+
+const academicFacultySchema = z.object({
+  name: z.string({ required_error: "Place create a faculty name!" }),
+});
+
 const CreateAcademicFaculty = () => {
-  return <div>CreateAcademicFaculty</div>;
+  const [addAcademicFaculty] = useAddAcademicFacultyMutation();
+
+  const onSubmit: SubmitErrorHandler<FieldValues> = async (data) => {
+    const toastId = toast.loading("Loading...");
+
+    const academicFacultyData = {
+      name: data.name,
+    };
+
+    try {
+      const res = (await addAcademicFaculty(academicFacultyData)) as TResponse;
+      if (res?.data) {
+        toast.error(res.data.message, { id: toastId });
+      }
+
+      if (res?.error) {
+        toast.error(res.error.data.message, { id: toastId });
+      }
+    } catch {
+      toast.error("Something went wrong!", { id: toastId });
+    }
+  };
+
+  return (
+    <>
+      <Flex justify="center" align="center">
+        <Col span={8}>
+          <PHForm
+            onSubmit={onSubmit}
+            resolver={zodResolver(academicFacultySchema)}
+          >
+            <PHInput type="text" name="name" label="Academic Faculty Name" />
+            <Button htmlType="submit">Submit</Button>
+          </PHForm>
+        </Col>
+      </Flex>
+    </>
+  );
 };
 
 export default CreateAcademicFaculty;
